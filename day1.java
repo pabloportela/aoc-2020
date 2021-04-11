a72613001874cbc8d56bf76309b26c80a2bf2c62
Pablo Gonzalez Portela <pablo.portela@gmail.com>
2021-04-11 13:56:32 +0200

    Refactored day1

--- day1.java
+++ day1.java
@@ -1,24 +1,37 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 
 public class day1 {
-
-    public static void main(String []args) throws IOException, NumberFormatException {
-        FileReader fileReader = new FileReader(args[0]);
+    
+    private static ArrayList<String> readLines(String filename) throws IOException {
+        FileReader fileReader = new FileReader(filename);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         String line;
-        Set<Integer> numbers = new HashSet<Integer>();
-        int target_sum = 2020;
+        ArrayList<String> lines = new ArrayList<String>();
+
+        while ((line = bufferedReader.readLine()) != null) {
+            lines.add(line);
+        }
+
+        return lines;
+    }
+
+    public static void main(String []args) throws NumberFormatException, IOException {
+
 
         // read the file numbers into a set
-        while ((line = bufferedReader.readLine()) != null) {
+        Set<Integer> numbers = new HashSet<Integer>();
+        for (String line: readLines(args[0])) {
             numbers.add(Integer.parseInt(line));
         }
 
+        // iterate and find first pair satisfying the equation "a + b = 2020"
+        int target_sum = 2020;
         for (Integer n: numbers) {
             if (numbers.contains(target_sum - n)) {
                 int solution = n * (target_sum - n);
