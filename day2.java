fa43e29386d43b6540d9beffbdfe9df7f71a1718
Pablo Gonzalez Portela <pablo.portela@gmail.com>
2021-04-11 21:28:48 +0200

    Day 2, both parts

--- day2.java
+++ day2.java
@@ -19,22 +19,10 @@ public class day2 {
     }
 
     public static void main(String []args) throws NumberFormatException, IOException {
-        part1(args[0]);
+        countValidPasswords(args[0]);
     }
 
-    private static boolean isLineValid(String line) throws NumberFormatException{
-            String[] parts = line.split(" ");
-            assert parts.length == 3;
-            String[] bounds = parts[0].split("-");
-            return isPasswordValid(
-                Integer.parseInt(bounds[0]),
-                Integer.parseInt(bounds[1]),
-                parts[1].charAt(0),
-                parts[2]
-            );
-    }
-
-    private static boolean isPasswordValid(int min_tokens, int max_tokens, char token, String password) {
+    private static boolean isValidPasswordPart1(int min_tokens, int max_tokens, char token, String password) {
         int q_tokens = 0;
         for (int i=0; i<password.length(); i++) {
             if (password.charAt(i) == token) {
@@ -45,18 +33,39 @@ public class day2 {
         return (q_tokens >= min_tokens && q_tokens <= max_tokens);
     }
 
-    private static void part1(String filename) throws NumberFormatException, IOException {
+    private static boolean isValidPasswordPart2(int first_pos, int second_pos, char token, String password) {
+        return
+            password.length() >= first_pos &&
+            password.length() >= second_pos &&
+            (password.charAt(first_pos-1) == token ^ password.charAt(second_pos-1) == token)
+        ;
+    }
+
+    private static void countValidPasswords(String filename) throws NumberFormatException, IOException {
         // read lines
         ArrayList<String> lines = readLines(filename);
 
         // parse and count valid ones
-        int q_valid = 0;
+        int q_valid_part_1 = 0;
+        int q_valid_part_2 = 0;
+
         for (String line: lines) {
-            if (isLineValid(line)) {
-                q_valid++;
+            // parse line
+            String[] parts = line.split(" ");
+            String[] bounds = parts[0].split("-");
+            int first = Integer.parseInt(bounds[0]);
+            int second = Integer.parseInt(bounds[1]);
+            char token = parts[1].charAt(0);
+
+            // count valid passwords for both parts
+            if (isValidPasswordPart1(first, second, token, parts[2])) {
+                q_valid_part_1++;
+            }
+            if (isValidPasswordPart2(first, second, token, parts[2])) {
+                q_valid_part_2++;
             }
         }
 
-        System.out.println("Number of valid passwords: " + q_valid);
+        System.out.println("Number of valid passwords for parts 1 and 2 respectively: " + q_valid_part_1 + " and " + q_valid_part_2);
     }
 }
