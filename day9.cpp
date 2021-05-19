590a5aabcc9a63e579dfa495e324a3e4a2b76e75
Pablo Gonzalez Portela <pablo.portela@gmail.com>
2021-05-19 12:08:33 +0200

    Day 9, part II

--- day9.cpp
+++ day9.cpp
@@ -59,6 +59,33 @@ Number find_first_non_compliant(const Numbers &numbers, const int preamble_size)
 }
 
 
+Number find_solution_for_part2(const Numbers &numbers, const Number target_sum) {
+    // fast-slow pointer
+    for (size_t i=0; i<numbers.size()-1; i++) {
+        // make sure we start with two numbers
+        Number min_number = min(numbers.at(i), numbers.at(i+1));
+        Number max_number = max(numbers.at(i), numbers.at(i+1));
+        Number current_sum = numbers.at(i) + numbers.at(i+1);
+
+        // expand until we either find the target or we go beyond it
+        size_t r_idx = i+1;
+        while (r_idx<numbers.size()-1 && current_sum < target_sum) {
+            r_idx++;
+            Number current_number = numbers.at(r_idx);
+            current_sum += current_number;
+            if (current_number < min_number) min_number = current_number;
+            if (current_number > max_number) max_number = current_number;
+        }
+
+        // found it
+        if (current_sum == target_sum) {
+            return min_number + max_number;
+        }
+    }
+
+    throw runtime_error("Unable to find sequence for part 2.");
+}
+
 int main(int argc, char **argv) {
     // gather params
     auto filename = argv[1];
@@ -70,6 +97,10 @@ int main(int argc, char **argv) {
     // part 1
     Number first_non_compliant = find_first_non_compliant(numbers, preamble_size);
     cout << "The first number which does not have such property is " << first_non_compliant << ".\n";
+
+    // part 2 and sorry did not find a better name
+    Number part2 = find_solution_for_part2(numbers, first_non_compliant);
+    cout << "Solution for part 2 is " << part2 << ".\n";
 
     return 0;
 }
